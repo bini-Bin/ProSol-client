b05e18a1ca31e0510d762eca2ec6ee5ab34ef6e8
binibin <jsb1129a@gmail.com>
2022-10-22 19:28:34 +0900

    first commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,22 @@
+const UnoCSS = require("@unocss/webpack").default;
+
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+
+  webpack(config, context) {
+    config.plugins.push(UnoCSS());
+
+    if (context.buildId !== "development") {
+      // * disable filesystem cache for build
+      // * https://github.com/unocss/unocss/issues/419
+      // * https://webpack.js.org/configuration/cache/
+      config.cache = false;
+    }
+
+    return config;
+  }
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
